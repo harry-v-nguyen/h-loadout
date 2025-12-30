5759e9260fc8c5b9d53ebf8bcca01bd1318dc469
Harry Nguyen <71202025+harry-v-nguyen@users.noreply.github.com>
2025-12-30 14:57:19 -0600

    feat: add telegram-desktop to omarchy

--- scripts/omarchy/install-all.sh
+++ scripts/omarchy/install-all.sh
@@ -9,6 +9,7 @@ echo -e "Installing specified packages...\n"
 ./scripts/omarchy/install/install-lsof.sh
 ./scripts/omarchy/install/install-opencode.sh
 ./scripts/omarchy/install/install-stow.sh
+./scripts/omarchy/install/install-telegram.sh
 ./scripts/omarchy/install/install-tmux.sh
 ./scripts/omarchy/install/install-veracrypt.sh
 ./scripts/omarchy/install/install-zen.sh
