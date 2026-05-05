179060f1da0fb5a32f446522366be9d835cf9fe5
Harry Nguyen <71202025+harry-v-nguyen@users.noreply.github.com>
2026-05-04 19:18:23 -0500

    feat: add nvim install in ubuntu-22.04

--- scripts/ubuntu-22.04/install-all.sh
+++ scripts/ubuntu-22.04/install-all.sh
@@ -7,7 +7,8 @@ echo -e "Installing specified packages...\n"
 ./scripts/ubuntu-22.04/install/install-bash-completion.sh
 ./scripts/ubuntu-22.04/install/install-fzf.sh
 ./scripts/ubuntu-22.04/install/install-go.sh
-./scripts/ubuntu-22.04/install/install-starship.sh
+./scripts/ubuntu-22.04/install/install-nvim.sh
+# ./scripts/ubuntu-22.04/install/install-starship.sh
 ./scripts/ubuntu-22.04/install/install-stow.sh
 ./scripts/ubuntu-22.04/install/install-tmux.sh
 
---
+++ scripts/ubuntu-22.04/install/install-nvim.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing nvim..."
+
+sudo snap install --classic nvim
